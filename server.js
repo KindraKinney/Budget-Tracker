e2e0d56f098544685da2da0ca829cfba7e5be843
Kindra Kinney <kindrakinney@gmail.com>
2020-04-17 17:25:43 -0700

    heroku deploy and final styling

--- server.js
+++ server.js
@@ -17,10 +17,14 @@ app.use(express.static("./public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
+
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true 
 });
+
+
 
 // routes
 app.use(require("./routes/api.js"));
